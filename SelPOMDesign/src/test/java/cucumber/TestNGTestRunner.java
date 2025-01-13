7d04e64a891b152aa668dee232f896757405907d
shaikchand786 <smchand4791@gmail.com>
2025-01-13 21:21:12 +0530

    changes done

--- SelPOMDesign/src/test/java/cucumber/TestNGTestRunner.java
+++ SelPOMDesign/src/test/java/cucumber/TestNGTestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/java/cucumber",	//Specifies the path to the feature files
 					glue = "ChandNaz/StepDefinition", //Specifies the package where step definitions (glue code) are located
 					monochrome = true, 				//Makes the console output more readable (removes unnecessary characters)
-					tags = "@tag1 @Regression",		//Runs only scenarios with the specified tag (e.g., @SmokeTest, @Regression)
+					tags = "@Test1 @Regression",		//Runs only scenarios with the specified tag (e.g., @SmokeTest, @Regression)
 					plugin = {"html: target/cucumber.html"})	//Defines plugins for reporting (such as HTML or JSON reports)
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
